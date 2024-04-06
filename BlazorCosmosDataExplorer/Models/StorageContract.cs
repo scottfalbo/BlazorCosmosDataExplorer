df70f140e6de295571049d5274ca87d9a31b16b9
Scott Falbo <scottfalboart@gmail.com>
2024-04-06 03:44:23 -0700

    made contract nullable

--- BlazorCosmosDataExplorer/Models/StorageContract.cs
+++ BlazorCosmosDataExplorer/Models/StorageContract.cs
@@ -5,29 +5,24 @@
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
 
-#nullable disable
-
 namespace BlazorCosmosDataExplorer.Models;
 
 [JsonObject(NamingStrategyType = typeof(CamelCaseNamingStrategy), ItemNullValueHandling = NullValueHandling.Ignore)]
 public class StorageContract
 {
-    public DateTimeOffset CreatedDate { get; set; }
+    public DateTimeOffset? CreatedDate { get; set; }
 
-    public string DocumentId { get; set; }
-
-    [JsonRequired]
-    public string Id { get; set; }
+    public string? DocumentId { get; set; }
 
     [JsonRequired]
-    public string Name { get; set; }
+    public string Id { get; set; } = string.Empty;
+
+    public string? Name { get; set; }
 
     [JsonRequired]
-    public string PartitionKey { get; set; }
+    public string PartitionKey { get; set; } = string.Empty;
 
-    public decimal Revenue { get; set; }
+    public decimal? Revenue { get; set; }
 
-    public DateTimeOffset UpdatedDate { get; set; }
+    public DateTimeOffset? UpdatedDate { get; set; }
 }
-
-#nullable enable
