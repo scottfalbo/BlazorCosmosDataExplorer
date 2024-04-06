b4f1c87a5d6a6d6d80f43178271c0c51cb5d49ca
Scott Falbo <scottfalboart@gmail.com>
2024-04-06 03:42:43 -0700

    made domainModel props nullable

--- BlazorCosmosDataExplorer/Models/DomainModel.cs
+++ BlazorCosmosDataExplorer/Models/DomainModel.cs
@@ -6,17 +6,17 @@ namespace BlazorCosmosDataExplorer.Models;
 
 public record DomainModel
 {
-    public DateTimeOffset CreatedDate { get; set; } = DateTimeOffset.UtcNow;
+    public DateTimeOffset? CreatedDate { get; set; }
 
-    public string DocumentId { get; set; } = string.Empty;
+    public string? DocumentId { get; set; }
 
-    public string Id { get; set; } = string.Empty;
+    public string? Id { get; set; }
 
-    public string Name { get; set; } = string.Empty;
+    public string? Name { get; set; }
 
-    public string PartitionKey { get; set; } = string.Empty;
+    public string? PartitionKey { get; set; }
 
-    public decimal Revenue { get; set; }
+    public decimal? Revenue { get; set; }
 
-    public DateTimeOffset UpdatedDate { get; set; } = DateTimeOffset.UtcNow;
+    public DateTimeOffset? UpdatedDate { get; set; }
 }
